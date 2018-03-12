677bb0805cdb85a51f6bcb797107f0877f2347ef
a.polden@gmail.com <Al P>
2018-03-12 22:19:57 +0000

    Add  process.env.PORT

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 
